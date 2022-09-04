32ef8a060c97ee93ac00c6459e23d121c720c73a
b0r1ngx <ghztrikz1337@gmail.com>
2022-09-04 12:29:39 +0300

    refactoring and adds to maxConsecutive.kt

--- src/yandexpracticum/maxConsecutive.kt
+++ src/yandexpracticum/maxConsecutive.kt
@@ -1,37 +1,57 @@
-val a = String()
-val b = mutableListOf<Int>()
+package yandexpracticum
 
-fun <T> findMaxConsecutive(): Int = TODO()
+fun <T> findNextOtherIndexElement(currIndex: Int, collection: Array<T>): Int {
+    var nextOtherValueIndex = currIndex
+
+    while (nextOtherValueIndex < collection.size &&
+        collection[nextOtherValueIndex] == collection[currIndex]
+    )
+        nextOtherValueIndex++
+
+    return nextOtherValueIndex
+}
 
 fun <T> maxConsecutiveElements(collection: Array<T>): Int {
     var result = 0
-    var currIndexElement = 0
+    var currIndex = 0
 
-    while (currIndexElement < collection.size) {
-        var nextIndexOtherElement = currIndexElement
-
-        while (nextIndexOtherElement < collection.size &&
-            collection[nextIndexOtherElement] == collection[currIndexElement]) {
-            nextIndexOtherElement++
-        }
-
-        result = result.coerceAtLeast(nextIndexOtherElement - currIndexElement)
-        currIndexElement = nextIndexOtherElement
+    while (currIndex < collection.size) {
+        val nextOtherValueIndex = findNextOtherIndexElement(currIndex, collection)
+        result = result.coerceAtLeast(nextOtherValueIndex - currIndex)
+        currIndex = nextOtherValueIndex
     }
 
     return result
 }
 
-fun maxConsecutiveChars(array: CharSequence): Int {
+fun findNextOtherValueIndex(currIndex: Int, sequence: CharSequence): Int {
+    var nextOtherValueIndex = currIndex
+
+    while (nextOtherValueIndex < sequence.length &&
+        sequence[currIndex] == sequence[nextOtherValueIndex]
+    )
+        nextOtherValueIndex++
+
+    return nextOtherValueIndex
+}
+
+fun maxConsecutiveChars(sequence: CharSequence): Int {
     var result = 0
-    var currIndexElement = 0
+    var currIndex = 0
 
-
+    while (currIndex < sequence.length) {
+        val nextOtherValueIndex = findNextOtherValueIndex(currIndex, sequence)
+        result = result.coerceAtLeast(nextOtherValueIndex - currIndex)
+        currIndex = nextOtherValueIndex
+    }
 
     return result
 }
 
 fun main() {
     val test = arrayOf(5, 5, 5, 4, 3, 2, 1, 1, 1, 1)
-    print(maxConsecutiveElements(test))
+    println(maxConsecutiveElements(test))
+
+    val testSequence = "privetbooms"
+    println(maxConsecutiveChars(testSequence))
 }
