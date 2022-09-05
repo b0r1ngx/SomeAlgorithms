39e2f37c4c82f76097edf18c7adfa1e19da20f83
b0r1ngx <ghztrikz1337@gmail.com>
2022-09-05 21:42:19 +0300

    end few next lessons (4, ...)

--- src/yandexpracticum/lesson4/recursionAndBacktracking.kt
+++ src/yandexpracticum/lesson4/recursionAndBacktracking.kt
@@ -1,2 +1,30 @@
 package yandexpracticum.lesson4
 
+/**
+ *  I knew a shortcut, with a formula !)
+ *  (thx Wiki, it's a really must have formula, u must see and know about it)
+ */
+
+fun fibonacci(n: Int): Int {
+    if (n <= 1)
+        return n
+
+    var (prev, curr) = 0 to 1
+
+    for (step in 0 until n)
+        prev = curr.also { curr += prev }
+
+    return curr
+}
+
+/**
+ * Это решение (выше) лучше рекурсии с мемоизацией,
+ * но если потребуется выполнять эту операцию несколько раз,
+ * то оно становится хуже.
+ *
+ * Решение проблемы: завести массив для посчитанных значений
+ */
+
+fun main() {
+    println(fibonacci(2))
+}
