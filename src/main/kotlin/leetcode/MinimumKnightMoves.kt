19ceb7adf0a7b07b8ef68145027de6a5a00644d7
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-29 00:05:00 +0300

    trying to solve MinimumKnightMoves (https://leetcode.com/problems/minimum-knight-moves/description/): 1. Worked for 3 of 4 test cases, in last case: infinity loop 2. No take care of optimizations

---
+++ src/main/kotlin/leetcode/MinimumKnightMoves.kt
@@ -0,0 +1,53 @@
+package leetcode
+
+data class Tile(val x: Int, val y: Int) {
+    // TODO: For any of figure? make it with best practices (using Factory Pattern)
+    fun availableTilesToMove(): List<Tile> {
+        return listOf()
+    }
+}
+
+class Knight {
+    fun minimumMoves(from: Tile, to: Tile): Int {
+        // check how it works if here is List, Map, etc...
+        val allAvailableTilesToVisit = mutableSetOf(from)
+
+        var moves = 0
+        while (!allAvailableTilesToVisit.contains(to)) {
+            var nextMoves: List<Tile> = listOf()
+            allAvailableTilesToVisit.forEach {
+                nextMoves = it.availableMoves()
+            }
+            moves += 1
+            allAvailableTilesToVisit.addAll(nextMoves)
+//            println(allAvailableTilesToVisit)
+        }
+
+        // TODO: val fromToPath: List<Tile>
+        return moves
+    }
+
+    private fun Tile.availableMoves(): List<Tile> {
+        return listOf(
+            copy(x = x + 1, y = y + 2),
+            copy(x = x + 2, y = y + 1),
+            copy(x = x + 2, y = y - 1),
+            copy(x = x + 1, y = y - 2),
+//        -- MIRRORED IN DIFF OF SIGN --
+            copy(x = x - 1, y = y - 2),
+            copy(x = x - 2, y = y - 1),
+            copy(x = x - 2, y = y + 1),
+            copy(x = x - 1, y = y + 2),
+        )
+    }
+}
+
+fun main() {
+    val from = Tile(0, 0)
+    val to1 = Tile(0, 0)
+    val to2 = Tile(1, 2)
+    val to3 = Tile(1, 1)
+    val to4 = Tile(2, 2)
+    val moves = Knight().minimumMoves(from, to4)
+    println(moves)
+}
