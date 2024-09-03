cbfc5dd442c974531aa04d7b19c757e2237934cf
Ivanov Kirill A <perkeboring@gmail.com>
2024-09-03 11:42:20 +0300

    provide task for problem

--- src/main/kotlin/leetcode/random/PartitionLabels.kt
+++ src/main/kotlin/leetcode/random/PartitionLabels.kt
@@ -1,5 +1,11 @@
 package leetcode.random
 
+/* You are given a string s. We want to partition the string into as many parts as possible
+ * so that each letter appears in at most one part.
+ * Note that the partition is done so that after concatenating all the parts in order,
+ * the resultant string should be s.
+ * Return a list of integers representing the size of these parts.
+ */
 fun partitionLabels(s: String): List<Int> {
     val result = mutableListOf<String>()
 
