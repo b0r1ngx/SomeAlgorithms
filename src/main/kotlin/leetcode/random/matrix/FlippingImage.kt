392aa078b0f0fec49074b882021ab517c6a50a20
Ivanov Kirill A <perkeboring@gmail.com>
2024-09-05 14:45:48 +0300

    solved task

---
+++ src/main/kotlin/leetcode/random/matrix/FlippingImage.kt
@@ -0,0 +1,29 @@
+package leetcode.random.matrix
+
+/* Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+ * To flip an image horizontally means that each row of the image is reversed.
+ * For example, flipping [1,1,0] horizontally results in [0,1,1].
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+ * For example, inverting [0,1,1] results in [1,0,0].
+ */
+fun flipAndInvertImage(image: Array<IntArray>): Array<IntArray> {
+    val result = mutableListOf<IntArray>()
+    for (row in image) {
+        val list = mutableListOf<Int>()
+        for (i in row.size - 1 downTo 0) {
+            val pixel = row[i]
+            when (pixel) {
+                1 -> list.add(0)
+                0 -> list.add(1)
+            }
+        }
+        result.add(list.toIntArray())
+    }
+    return result.toTypedArray()
+}
+
+fun main() {
+    println(flipAndInvertImage(arrayOf(intArrayOf(1))))
+    // Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+    // Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+}
