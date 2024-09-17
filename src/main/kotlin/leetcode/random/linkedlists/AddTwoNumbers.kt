f3e0ec34e5bf68ee1dda05c4a92a41acec6b663a
Ivanov Kirill A <perkeboring@gmail.com>
2024-09-18 00:39:49 +0300

    solved task

--- src/main/kotlin/leetcode/random/linkedlists/AddTwoNumbers.kt
+++ src/main/kotlin/leetcode/random/linkedlists/AddTwoNumbers.kt
@@ -14,9 +14,79 @@ class ListNode(var `val`: Int) {
 }
 
 fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-    return ListNode(0)
+    var l1node = l1
+    var l2node = l2
+
+    var firstNumber = 0
+    var secondNumber = 0
+
+    var currFirstDigit: Int
+    var currSecondDigit: Int
+    var currentDigit = 1
+
+    var isBothNumbersNotEnd = true
+    while (isBothNumbersNotEnd) {
+        currFirstDigit = l1node?.`val` ?: 0
+        currSecondDigit = l2node?.`val` ?: 0
+
+        firstNumber += currFirstDigit * currentDigit
+        secondNumber += currSecondDigit * currentDigit
+
+        l1node = l1node?.next
+        l2node = l2node?.next
+
+        if (l1node == null && l2node == null) {
+            isBothNumbersNotEnd = false
+        }
+
+        currentDigit *= 10
+    }
+
+    val preResult = (firstNumber + secondNumber).toDigits()
+    println(preResult)
+    val preResultSize = preResult.size - 1
+
+    val result = ListNode(preResult[preResultSize])
+    var nextRes = result
+    for (i in preResultSize - 1 downTo 0) {
+        val value = preResult[i]
+        val node = ListNode(value)
+        nextRes.next = node
+        nextRes = node
+    }
+
+    return result
 }
 
+fun Int.toDigits() = toString().map { it.toString().toInt() }
+
 fun main() {
+//    val l1 = ListNode(2)
+//    val nextl1 = ListNode(4)
+//    nextl1.next = ListNode(3)
+//    l1.next = nextl1
+//
+//    val l2 = ListNode(5)
+//    val nextl2 = ListNode(6)
+//    nextl2.next = ListNode(4)
+//    l2.next = nextl2
+//    println(addTwoNumbers(l1 = l1, l2 = l2)?.next?.`val`)
+
+
+    val l1 = ListNode(9)
+
+    val L2head = ListNode(1)
+    val nodeValues = listOf(1, 9, 9, 9, 9, 9, 9, 9, 9, 9)
+    var nextL2 = L2head
+    for (i in 1 until nodeValues.size - 1) {
+        val value = nodeValues[i]
+        val node =  ListNode(value)
+        nextL2.next = node
+        nextL2 = node
+    }
+
+    // Output: [8,0,4,5,6,0,0,1,4,1]
+    // Expected: [0,0,0,0,0,0,0,0,0,0,1]
+    val result = addTwoNumbers(l1 = l1, l2 = L2head)
 
 }
