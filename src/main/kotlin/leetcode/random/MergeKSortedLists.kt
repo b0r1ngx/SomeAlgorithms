8d7203bba26ab71156c5980677668e8ad9316552
Ivanov Kirill A <perkeboring@gmail.com>
2024-10-08 21:46:49 +0300

    solved merge k sorted lists (not tested via leetcode)

--- src/main/kotlin/leetcode/random/MergeKSortedLists.kt
+++ src/main/kotlin/leetcode/random/MergeKSortedLists.kt
@@ -1,46 +1,55 @@
 package leetcode.random
 
+import leetcode.datalayer.ListNode
+
 /* You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
  * Merge all the linked-lists into one sorted linked-list and return it.
  */
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
+fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    for (list in lists) println(list)
+    if (lists.isEmpty()) return null
 
-class MergeKSortedLists {
-    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-        if (lists.isEmpty()) {
-            return null
-        }
+    var isNotSorted = true
+    var result: ListNode? = null
+    while (isNotSorted) {
+        var currMinValue = Int.MAX_VALUE
+        var currMinValueIndex = 0
 
-        val result = mutableListOf<Int>()
-        var currMin = 10_000 // max constraint by task, can be replaced by Int.MAX_VALUE
-        for (list in lists) {
-            val firstListValue = list?.`val` ?: continue
-            if (firstListValue <= currMin) {
-                currMin = firstListValue
+        for (index in lists.indices) {
+            val node = lists[index]
+            val value = node?.`val` ?: continue
+            if (value < currMinValue) {
+                currMinValue = value
+                currMinValueIndex = index
             }
         }
 
-        val isNotSorted = true
-        var nextMin = currMin
-        while (isNotSorted) {
-            lists.forEach {
+        if (currMinValue == Int.MAX_VALUE) break
 
-            }
-        }
-        lists.forEach {
+        if (result == null) result = ListNode(currMinValue)
+        else result.appendNode(ListNode(currMinValue))
 
-        }
-
-        return ListNode(0)
-//        return result.forEach {  }
+        lists[currMinValueIndex] = lists[currMinValueIndex]?.next
     }
+
+    return result
 }
 
 fun main() {
-//    val lists = Array<ListNode?>{
-//
-//    }[[1,4,5],[1,3,4],[2,6]] // [1,1,2,3,4,4,5,6]
-//    MergeKSortedLists().mergeKLists()
+    val l1 = ListNode(1)
+    val nextl1 = ListNode(4)
+    nextl1.next = ListNode(5)
+    l1.next = nextl1
+
+    val l2 = ListNode(1)
+    val nextl2 = ListNode(3)
+    nextl2.next = ListNode(4)
+    l2.next = nextl2
+
+    val l3 = ListNode(2)
+    l3.next = ListNode(6)
+    val lists: Array<ListNode?> = arrayOf(l1, l2, l3) // [1,1,2,3,4,4,5,6]
+
+    var output = mergeKLists(lists)
+    println(output)
 }
