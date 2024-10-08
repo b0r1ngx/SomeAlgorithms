fb3c727418cc30a6a9fa7726208524b51d20e778
Ivanov Kirill A <perkeboring@gmail.com>
2024-10-08 21:47:41 +0300

    created ListNode as separate class, because it is used in more than one task at leetcode

---
+++ src/main/kotlin/leetcode/datalayer/ListNode.kt
@@ -0,0 +1,23 @@
+package leetcode.datalayer
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+
+    fun appendNode(node: ListNode) {
+        var lastNode: ListNode = this
+        while (lastNode.next != null) {
+            lastNode = lastNode.next!!
+        }
+        lastNode.next = node
+    }
+
+    override fun toString(): String {
+        var output = ""
+        var node: ListNode? = this
+        while (node != null) {
+            output += "${node.`val`}->"
+            node = node.next
+        }
+        return output
+    }
+}
