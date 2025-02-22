2ee9176fef054daf001fbf69afe058e06e23d885
Ivanov Kirill A <perkeboring@gmail.com>
2025-02-22 15:31:30 +0300

    solved task optimal

--- src/main/kotlin/yandex/findPathInFlights.kt
+++ src/main/kotlin/yandex/findPathInFlights.kt
@@ -18,6 +18,10 @@ fun findPath(from: String, to: String, fetchFlights: (fromCity: String) -> List<
 
 // задача с измененными входными данными
 // передаем graph, вместо функции fetchFlights
+
+// what if graph is: {A: [B], B: [C], C: [D], D: [E], E: [F], F: [G]}
+// find from A -> G
+// bad: memory O(n*n/2) -> O(n^2)
 fun findPath(from: String, to: String, graph: Map<String, List<String>>): List<String> {
     val paths = mutableMapOf<String, List<String>>()
 
@@ -32,18 +36,63 @@ fun findPath(from: String, to: String, graph: Map<String, List<String>>): List<S
         for (end in possibleEnds) {
             if (end !in paths) stack.push(end)
             paths[end] = currentRoute.plus(end)
-            if (to == end) return paths[end]!!.also { println(it) }
+            if (to == end) return paths[end]!!.also {
+                println(paths)
+                println(it)
+            }
         }
     }
     return listOf()
 }
 
+fun findPathOptimal(from: String, to: String, graph: Map<String, List<String>>): List<String> {
+    val paths = mutableMapOf<String, String>()
+
+    val stack = Stack<String>()
+    stack.push(from)
+
+    while (stack.isNotEmpty()) {
+        val node = stack.pop()
+        val possibleEnds = graph[node] ?: continue
+        for (end in possibleEnds) {
+            if (end !in paths) stack.push(end)
+            paths[end] = node
+            if (to == end) return createPath(from, to, paths)
+        }
+    }
+    return listOf()
+}
+
+private fun createPath(from: String, to: String, paths: Map<String, String>): List<String> {
+    println(paths)
+    var prevFlight = paths[to] ?: return listOf(to)
+    val result = mutableListOf(to, prevFlight)
+
+    while (prevFlight !== from) {
+        prevFlight = paths[prevFlight]!!
+        result.add(prevFlight)
+    }
+
+    return result.reversed()
+}
+
 fun main() {
-    val graph = mutableMapOf(
+    var graph = mutableMapOf(
         "A" to listOf("B", "D"), "B" to listOf("C", "N", "Z"),
         "D" to listOf("E", "F"), "F" to listOf("S"),
     )
-    println(listOf("A", "B", "N") == findPath("A", "N", graph)) // Promise.resolve(['A', 'B', 'N']) -> not empty list
-    println(listOf("A", "D", "F", "S") == findPath("A", "S", graph)) // Promise.resolve(['A', 'D', 'F', 'S']) -> not empty list
-    println(listOf<String>() == findPath("B", "S", graph)) // Promise.reject(new Error( 'No way')) -> empty list
+    println("graph: $graph")
+    println(listOf("A", "B", "N") == findPathOptimal("A", "N", graph))
+    println(listOf("A", "D", "F", "S") == findPathOptimal("A", "S", graph))
+    println(listOf<String>() == findPathOptimal("B", "S", graph))
+
+    graph = mutableMapOf(
+        "A" to listOf("B"), "B" to listOf("C"), "C" to listOf("D"),
+        "D" to listOf("E"), "E" to listOf("F"), "F" to listOf("G")
+    )
+    println("graph: $graph")
+    // memory with findPath(...):
+    // keys (keys+values) -> values (keys+values)
+    // 6 (12) -> 28 (35)
+    println(listOf("A", "B", "C", "D", "E", "F", "G") == findPathOptimal("A", "G", graph))
 }
