1465e39d37c59cdeba85527a6034307d30b9efb0
Ivanov Kirill A <perkeboring@gmail.com>
2025-02-23 17:02:28 +0300

    1) override equals & hashcode, to correct comparing; 2) add new constructor;

--- src/main/kotlin/leetcode/binarytree/TreeNode.kt
+++ src/main/kotlin/leetcode/binarytree/TreeNode.kt
@@ -3,4 +3,22 @@ package leetcode.binarytree
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+    constructor(`val`: Int, left: TreeNode?, right: TreeNode?) : this(`val`) {
+        this.`val` = `val`
+        this.left = left
+        this.right = right
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TreeNode) return false
+        return `val` == other.`val` && left == other.left && right == other.right
+    }
+
+    override fun hashCode(): Int {
+        var result = `val`.hashCode()
+        result = 31 * result + left.hashCode() + right.hashCode()
+        return result
+    }
 }
