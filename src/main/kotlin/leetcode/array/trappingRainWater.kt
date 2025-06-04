00c022ede36d961c7f0f9f82b84cf36fb4e182b3
Ivanov Kirill A <perkeboring@gmail.com>
2025-06-04 22:52:34 +0300

    using `array` instead of `list` naming for directory

--- src/main/kotlin/leetcode/array/trappingRainWater.kt
+++ src/main/kotlin/leetcode/array/trappingRainWater.kt
@@ -1,4 +1,4 @@
-package leetcode.list
+package leetcode.array
 
 // Given n non-negative integers representing an elevation map where the width of each bar is 1,
 // compute how much water it can trap after raining.
